26522e3f7433018becf1de2257e53c2bd95d9c5e
Пристрій УЦОЯО <t2252@mon.gov.ua>
2023-01-26 15:36:46 +0200

    Форма запису React.Fragment

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-/*const h1 = React.createElement('h1', { id: 'title' }, 'Hello React')
+const Title = () => {
+    return <h1>Hello App.js</h1>
+}
+const Content = () => {
+    return (
+        <React.Fragment>
+            <p>Lorem </p>
+            <p>Lorem </p>
+        </React.Fragment>
+    )
+}
 
-/*const list = React.createElement(
-    'ul',
-    null,
-    React.createElement('li', null, 'list Item 1'),
-    React.createElement('li', null, 'list Item 2'),
-    React.createElement('li', null, 'list Item 3')
-)*/
-
-//jsx
-const h1 = (
-    <h1 id="title" className="dfdf">
-        Hello React.js
-    </h1>
-)
-console.log(h1)
-
-let a = 10
-let b = 'Hello test'
-const list = (
-    <ul>
-        <li>List item {a + 10 + 10}</li>
-        <li>List item {b}</li>
-        <li>List item 3</li>
-    </ul>
-)
-const content = (
-    <div>
-        {h1}
-        {list}
-    </div>
-)
+function App() {
+    return (
+        <div>
+            <Title />
+            <Content />
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{content}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
