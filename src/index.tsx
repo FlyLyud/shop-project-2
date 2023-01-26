2462ebed05bff4d16d00d84eabb733acf86fb8f7
Пристрій УЦОЯО <t2252@mon.gov.ua>
2023-01-26 11:35:57 +0200

    Створення реакт елементівз jsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom/client'
 
 const h1 = React.createElement('h1', { id: 'title' }, 'Hello React')
 
-const list = React.createElement(
+/*const list = React.createElement(
     'ul',
     null,
     React.createElement('li', null, 'list Item 1'),
     React.createElement('li', null, 'list Item 2'),
     React.createElement('li', null, 'list Item 3')
-)
+)*/
 
+const list = (
+    <ul>
+        <li>List item 1</li>
+        <li>List item 2</li>
+        <li>List item 3</li>
+    </ul>
+)
 const content = (
     <div>
         {h1}
