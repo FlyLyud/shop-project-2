27a9cfae2b9aa8fe1abd083b01d0cac94cf9d455
Пристрій УЦОЯО <t2252@mon.gov.ua>
2023-01-28 13:38:07 +0200

    Компоненти і їх типи props

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const Title = () => {
-    return <h1>Hello App.js</h1>
+interface ITitleProps {
+    text: string
 }
-const Content = () => {
+type ContentProps = {
+    text1: string
+    text2: string
+    year: number
+}
+
+const Title = (props: ITitleProps) => {
+    console.log(props)
+    return <h1>Hello {props.text}</h1>
+}
+const Content = (props: ContentProps) => {
     return (
         <React.Fragment>
-            <p>Lorem </p>
-            <p>Lorem </p>
+            <p>{props.text1} </p>
+            <p>{props.text2} </p>
+            <div>Year:{props.year}</div>
         </React.Fragment>
     )
 }
 
 function App() {
     return (
-        <div>
-            <Title />
-            <Content />
-        </div>
+        <>
+            <Title text="React" />
+            <Title text="TS" />
+            <Content text1="Hello world1" text2="Hello world2" year={2023} />
+        </>
     )
 }
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
