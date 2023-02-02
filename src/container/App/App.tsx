7af643d4b5f36b1a16f9d03ad9171ec1d95cd520
Пристрій УЦОЯО <t2252@mon.gov.ua>
2023-02-02 12:15:03 +0200

    add App,Header,Main,Footer

---
+++ src/container/App/App.tsx
@@ -0,0 +1,17 @@
+import Footer from 'container/Footer/Footer'
+import Header from 'container/Header/Header'
+import Main from 'container/Main/Main'
+import CssBaseline from '@mui/material/CssBaseline'
+
+type Props = {}
+
+const App = (props: Props) => {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    )
+}
+export default App
