ac3307c37c34d0454c56d8ed221fc71629eb06d1
Пристрій УЦОЯО <t2252@mon.gov.ua>
2023-02-03 12:16:17 +0200

    step1

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -5,13 +5,14 @@ import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import { Container } from '@mui/material'
+import './Header.scss'
 
 type Props = {}
 const Header = (props: Props) => {
     return (
         // <div>Header</div>
-        <AppBar position="static">
-            <Container maxWidth="md">
+        <AppBar position="static" className="app-bar">
+            <Container>
                 <Toolbar>
                     <IconButton
                         size="large"
